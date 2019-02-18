eee979c08e434eface4f67256b7a2e153f1349eb
Hoang Ngoc Hai <hoang.ngoc.hai@framgia.com>
2019-02-18 18:34:13 +0700

    Panigation Books

--- src/main/java/com/framgia/bookStore/repository/BookReponsitory.java
+++ src/main/java/com/framgia/bookStore/repository/BookReponsitory.java
@@ -4,6 +4,7 @@ import com.framgia.bookStore.entity.BookEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,8 +15,17 @@ public interface BookReponsitory extends CustomJpaRepository<BookEntity, Long>{
 
     // Query get TOP 5 books most sell
     @Query(value = "SELECT b FROM BookEntity b JOIN b.orderDetails d WHERE b.deleted = FALSE GROUP BY b.id ORDER BY SUM(d.quantity) DESC")
-    Page<BookEntity> getTop5Book(Pageable pageable);
+    List<BookEntity> getTop5Book(Pageable pageable);
 
     @Query(value = "FROM BookEntity b ORDER BY b.id DESC")
     List<BookEntity> get10Books(Pageable pageable);
+
+    @Query("SELECT b FROM BookEntity b WHERE b.name LIKE %:name%")
+    Page<BookEntity> getByName(Pageable pageable, @Param("name") String name);
+
+    @Query("SELECT b FROM BookEntity b WHERE b.price BETWEEN :num1 AND :num2")
+    Page<BookEntity> getAllByPriceBetween(Pageable pageable, long num1, long num2);
+
+    @Query("SELECT b FROM BookEntity b JOIN b.authors a WHERE a.name LIKE %:author%")
+    Page<BookEntity> getByAuthor(Pageable pageable, @Param("author") String author);
 }
--- src/main/java/com/framgia/bookStore/service/BookSerive.java
+++ src/main/java/com/framgia/bookStore/service/BookSerive.java
@@ -2,12 +2,17 @@ package com.framgia.bookStore.service;
 
 import com.framgia.bookStore.entity.BookEntity;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface BookSerive {
     List<BookEntity> loadAll();
     BookEntity getByAliasName(String aliasName);
-    Page<BookEntity> getTop5Book();
+    List<BookEntity> getTop5Book();
     List<BookEntity> get10Books();
+    Page<BookEntity> findAll(Pageable pageable);
+    Page<BookEntity> findAllByName(Pageable pageable, String name);
+    Page<BookEntity> findAllByPrice(Pageable pageable, Long num1, Long num2);
+    Page<BookEntity> findAllByAuthor(Pageable pageable, String author);
 }
